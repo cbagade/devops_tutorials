6ae3dc333c1aa51dc426e0c2b59d90c6698b8a2c
Chandra <cbagade@yahoo.com>
2024-03-13 08:05:19 +0530

    changes

--- nodejs-learnings/13_03.js
+++ nodejs-learnings/13_03.js
@@ -1,11 +1,9 @@
 const fs = require('fs')
 
 
-function my_call_back(err,data){
+fs.readFile('./data/course.txt','utf-8',function my_call_back(err,data){
     console.log(`${data}`)
-}
-
-fs.readFile('./data/course.txt','utf-8',my_call_back)
+})
 
 
 console.log('I want to go movie\n')
