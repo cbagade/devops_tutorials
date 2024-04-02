6f8f2d2a4930cbf7477ffe9cde0cdf9737e583f8
Chandra <cbagade@yahoo.com>
2024-04-02 07:28:15 +0530

    changes

--- nodejs-learnings/fruit_contorller_file.js
+++ nodejs-learnings/fruit_contorller_file.js
@@ -1,39 +1,23 @@
 const fs = require("fs")
-const Fruit = require('./express_demos/models/fruit_model')
 
-//const fruits = fs.readFileSync('./data/fruits_data.json','utf-8')
-//const fruit_json = JSON.parse(fruits)
-
-
-exports.get_all_fruits = async(req,res)=>{
+exports.get_all_fruits = (req,res)=>{
     console.log('I am coming from method')
-    const all_fruits = await Fruit.find()
+    const all_fruits = {}
     res.status(200).json({message:'success', fruits:all_fruits})
 }
  
-exports.get_a_fruit = async(req,res) =>{
+exports.get_a_fruit = (req,res) =>{
     console.log('I am coming from method')
     const id = req.params.id
-    //const fruit = fruit_json[id]
-    const one_fruit = await Fruit.findById(id)
+    one_fruit = {}
     res.status(200).json({ status: "success", data: { one_fruit } });
 }
 
 
-exports.create_a_fruit = async (req,res) =>{
+exports.create_a_fruit = (req,res) =>{
     const fruit = req.body
     console.log(`fruit ${fruit}`)
-
-    const new_fruit = Fruit.create(req.body)
+    const new_fruit = {}
     res.status(200).json({ status: "success", data: { new_fruit } });
     
-}
-
-exports.validateid = (req,res,next) => {
-//    const id = req.params.id * 1
-  //  if(id>=fruit_json.length){
-    //    return res.status(400).send("invalid id")
-   // }
-    next()
-
 }
--- nodejs-learnings/server_file.js
+++ nodejs-learnings/server_file.js
@@ -1,19 +1,4 @@
 const app = require("./27_01")
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-
-dotenv.config({path:'./express_demos/config.env'})
-
-const db = process.env.DATABASE
-console.log(db);
-
-
-(
-    async() =>{
-        await mongoose.connect(db)
-        console.log('connection success')
-    }
-)();
 
 
 ///////////////// SERVER  ////////////////////////
